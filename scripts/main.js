1f6d53ec7719554abff3823bae872090654bdaf2
Antiselekotin <deksazon8@gmail.com>
2020-04-28 19:36:52 +0300

    second

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
-const burger = document.querySelector('.burger');
-const burger__item = document.querySelectorAll('.burger__item')
-const navbar__list = document.querySelector('.navbar__list')
-const navbar__lang = document.querySelector('.navbar__lang')
+document.addEventListener('DOMContenrLoad', () => {
+    const burger = document.querySelector('.burger');
+    const burger__item = document.querySelectorAll('.burger__item')
+    const navbar__list = document.querySelector('.navbar__list')
+    const navbar__lang = document.querySelector('.navbar__lang')
 
-burger.addEventListener('click', () => {
-    burger__item.forEach(item => {
-        item.classList.toggle('burger__item_active');
+    burger.addEventListener('click', () => {
+        burger__item.forEach(item => {
+            item.classList.toggle('burger__item_active');
+        })
+        navbar__list.classList.toggle('navbar__list_active');
+        navbar__lang.classList.toggle('navbar__lang_active');
     })
-    navbar__list.classList.toggle('navbar__list_active');
-    navbar__lang.classList.toggle('navbar__lang_active');
+
+    const video = document.querySelector('.video');
+    console.log(video)
 })
+
+
