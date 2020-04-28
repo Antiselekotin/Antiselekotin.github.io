603d4469ac4b2a5cb1ff8fbc094ed66a95a5b42d
Antiselekotin <deksazon8@gmail.com>
2020-04-28 18:50:33 +0300

    first

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger');
+const burger__item = document.querySelectorAll('.burger__item')
+const navbar__list = document.querySelector('.navbar__list')
+const navbar__lang = document.querySelector('.navbar__lang')
+
+burger.addEventListener('click', () => {
+    burger__item.forEach(item => {
+        item.classList.toggle('burger__item_active');
+    })
+    navbar__list.classList.toggle('navbar__list_active');
+    navbar__lang.classList.toggle('navbar__lang_active');
+})
