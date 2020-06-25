26ce2100d9e2ac49eedf53b78d973fa1aa9e69de
Antiselekotin <deksazon8@gmail.com>
2020-06-25 13:54:16 +0300

    star db

---
+++ precache-manifest.ff36d98c39a4c28357fd85947d1c17ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343aa0b729d6b683700ad756eefd57e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f7b76d1af2762bdb49",
+    "url": "/static/css/main.13fcca22.chunk.css"
+  },
+  {
+    "revision": "2730810fa22cb4ede092",
+    "url": "/static/js/2.cd45db0f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.cd45db0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f7b76d1af2762bdb49",
+    "url": "/static/js/main.ad59dba0.chunk.js"
+  },
+  {
+    "revision": "67dcc306d6d3f6b64783",
+    "url": "/static/js/runtime-main.f674874b.js"
+  },
+  {
+    "revision": "c6f8cd725c3d7ec8eb7454b9d515d620",
+    "url": "/static/media/star.c6f8cd72.png"
+  }
+]);
